ae241cc84b64e885065c0f9e2a6d6f34bab7d6a0
Abdulqudus <abdullateefqudusleeq@gmail.com>
2026-01-20 21:12:07 +0100

    Update Hero.tsx

--- my-app/components/Hero.tsx
+++ my-app/components/Hero.tsx
@@ -9,7 +9,7 @@ export default function Hero() {
       {/* Background Image */}
       <div className="absolute inset-0 z-0">
         <Image
-          src="/images/hero-consultant.jpg"
+          src="/hero-consultant.jpg"
           alt="Business consulting"
           fill
           className="object-cover"
